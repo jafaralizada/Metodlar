b770aec88be7812b161dd16e5de6127a38650f39
jraa <ceferelizade@gmail.com>
2021-01-16 23:42:47 +0300

    Add project files.

---
+++ Metodlar/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elmasi";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2};
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------");
+
+            }
+            Console.WriteLine("-------------Metodlar-------------");
+            //instance - ornek
+            //encapsulation
+
+
+
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yesil Elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakir karpuzu", 12, 8);
+
+
+        }
+    }
+}
